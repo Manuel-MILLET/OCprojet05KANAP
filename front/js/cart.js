729c316763f4c4d9eb2b6b846811444ed2209b63
Manuel MILLET <manuel.millet.ovh@gmail.com>
2022-07-04 08:52:30 +0200

    script d affichage de la liste des produits dans la page d'accueil

---
+++ front/js/cart.js
@@ -0,0 +1,8 @@
+/*
+Script Porjet 05 Kanap par Manuel MILLET
+
+Script de la page cart.html pour l'affichage dynamique de produits
+
+*/
+
+alert ("Script de la page cart");
---
+++ front/js/confirmation.js
@@ -0,0 +1,8 @@
+/*
+Script Porjet 05 Kanap par Manuel MILLET
+
+Script de la page confirmation.html pour l'affichage dynamique de produits
+
+*/
+
+alert ("Script de la page confirmation.html");
